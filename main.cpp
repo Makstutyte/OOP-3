9dea526683502ce64127e472e9100562382b9d85
Makstutyte <57039046+Makstutyte@users.noreply.github.com>
2020-04-18 10:59:34 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,9 +7,9 @@
 int main ()
 {
     char konteineris1; std::cout <<std::endl;
+    Studentas1 lol;
     std::cout << "vector (v) / list (l) / deque (d)? --> "; std::cin >> konteineris1;
     konteineris (konteineris1, 'v', 'l', 'd');
-
     if (konteineris1=='v')
     {
         vektorius();
@@ -25,6 +25,5 @@ int main ()
                kazkas_is_d_raides();
           }
             
-
     return 0;
 }
