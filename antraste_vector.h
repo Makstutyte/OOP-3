6f0c1b3ad080c2d757e6c1ab5d6ece4709b8c633
Makstutyte <57039046+Makstutyte@users.noreply.github.com>
2020-04-18 10:57:57 +0300

    Update antraste_vector.h

--- antraste_vector.h
+++ antraste_vector.h
@@ -15,25 +15,52 @@
 
 const int daugiausia = 50;
 
-struct Studentas1
+class Studentas1
 {
-    std::string vardas, pavarde;
-    int egz; int n = 0;
-    double galutinis;
-    std::vector<int> vektoriukas;
+
+  public:
+
+   inline Studentas1() : vardas("vardas"), pavarde("pavarde"), egz(0) { }
+
+   /*
+   inline Studentas1() 
+   {
+     	int egz = 0;
+   }
+   */
+
+   inline ~Studentas1(){ }
+
+    void setName(std::string);
+    void setSurname(std::string);
+    void setExam(int);
+
+    inline std::string getName() const {return vardas; };
+    inline std::string getSurname() const { return pavarde; };
+    inline int getExam() const { return egz;};
+    
+      int n = 0;
+      double galutinis;
+      std::vector<int> vektoriukas;
+
+  private:
+
+      std::string vardas;
+      std::string pavarde;
+      int egz; 
 };
 
 void konteineris (char& simbolis, char pirmas, char antras, char trecias);
 void ar_tas_simboliukas (char& simbolis, char pirmas, char antras);
 void ar_tinkamas_skaiciukas (int& skaicius, int pradzia, int pabaiga);
 double rastimediana (std::vector<int> vekt);
-double vidurkis (int* nd, int n);
-double galutinis (char kas_cia1, Studentas1* Studentas);
-double galutinis_skaitant (char kas_cia1, Studentas1* Studentas, std::vector<int> vektoriukas);
-void pazymiu_generavimas (char kas_cia, Studentas1* Studentas);
-bool rikiuojam(Studentas1 a, Studentas1  b);
-void I_FAILA_pazymiu_generavimas (Studentas1* Studentas, std::string pavadinimas, int kiek);
-bool rikiuojam_pagal_bala(Studentas1 a, Studentas1  b);
+double vidurkis (int const* nd, int n);
+double galutinis (char kas_cia1,  Studentas1& Studentas);
+double galutinis_skaitant (char kas_cia1, Studentas1& Studentas, std::vector<int> vektoriukas);
+void pazymiu_generavimas (char kas_cia,  Studentas1& Studentas);
+bool rikiuojam( Studentas1& a, Studentas1& b);
+void I_FAILA_pazymiu_generavimas ( Studentas1& Studentas, std::string pavadinimas, int kiek);
+bool rikiuojam_pagal_bala( Studentas1& a,  Studentas1& b);
 void vektorius();
 
 #endif // ANTRASTE_VECTOR_H_INCLUDED
