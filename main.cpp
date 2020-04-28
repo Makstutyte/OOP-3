cdd0f0a093de8b40e7918a5f76aec91806bb023b
Makstutyte <57039046+Makstutyte@users.noreply.github.com>
2020-04-28 20:59:03 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,12 +7,14 @@
 int main ()
 {
     char konteineris1; std::cout <<std::endl;
+    Studentas1 lol; 
+    Timer t;
     std::cout << "vector (v) / list (l) / deque (d)? --> "; std::cin >> konteineris1;
     konteineris (konteineris1, 'v', 'l', 'd');
-
     if (konteineris1=='v')
     {
         vektorius();
+        std::cout << t.elapsed() <<std::endl;
     }
      
      else if (konteineris1=='l')
@@ -25,6 +27,5 @@ int main ()
                kazkas_is_d_raides();
           }
             
-
     return 0;
 }
